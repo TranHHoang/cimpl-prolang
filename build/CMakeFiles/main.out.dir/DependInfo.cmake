aabc1ffffa7533f8f15aa5436841846ff53bfe01
Tran Hoang <tranhoang345@gmail.com>
2018-09-30 09:58:56 +0700

    add build system

---
+++ build/CMakeFiles/main.out.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hoang/code/cimpl-prolang/src/list/vector.c" "/home/hoang/code/cimpl-prolang/build/CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/list/vector.c.o"
+  "/home/hoang/code/cimpl-prolang/src/main/main.c" "/home/hoang/code/cimpl-prolang/build/CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/main/main.c.o"
+  "/home/hoang/code/cimpl-prolang/src/token/token.c" "/home/hoang/code/cimpl-prolang/build/CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/token/token.c.o"
+  "/home/hoang/code/cimpl-prolang/src/token/tokstream.c" "/home/hoang/code/cimpl-prolang/build/CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/token/tokstream.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.out.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/main/main.c.o"
+  "CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/list/vector.c.o"
+  "CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/token/token.c.o"
+  "CMakeFiles/main.out.dir/home/hoang/code/cimpl-prolang/src/token/tokstream.c.o"
+  "main.out.pdb"
+  "main.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
